5b576e50aff564a5f8294c8b7859d0e2f3e6b0cf
Japangular <Japangular@gmx.de>
2025-05-20 18:46:14 +0200

    changed swagger, fixed gitignore and generated files in frontend

--- FrontendQuiz/src/app/api/model/models.ts
+++
@@ -1,2 +0,0 @@
-export * from './propertyType';
-export * from './submissionDeck';
---
+++ FrontendQuiz/src/app/features/deck-chooser/deck-chooser.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeckChooserComponent } from './deck-chooser.component';
+
+describe('DeckChooserComponent', () => {
+  let component: DeckChooserComponent;
+  let fixture: ComponentFixture<DeckChooserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DeckChooserComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DeckChooserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
