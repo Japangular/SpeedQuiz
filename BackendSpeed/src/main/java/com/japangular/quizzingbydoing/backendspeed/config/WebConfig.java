6e6d7b990f9f2f5c78871ea984cb701e097373b2
Japangular <Japangular@gmx.de>
2026-05-24 15:05:16 +0200

    fix webconfig

--- BackendSpeed/src/main/java/com/japangular/quizzingbydoing/backendspeed/config/WebConfig.java
+++ BackendSpeed/src/main/java/com/japangular/quizzingbydoing/backendspeed/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.japangular.quizzingbydoing.backendspeed.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,10 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+  @Value("${app.cors.allowed-origins}")
+  private String[] allowedOrigins;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:4200", "http://localhost:4300")
+        .allowedOrigins(allowedOrigins)
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
